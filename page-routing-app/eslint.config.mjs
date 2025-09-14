49d4fd51929d3e44679177c98deddd8b2a9e563f
worldbrighteststar <scott.choi@gmail.com>
2025-09-15 01:20:11 +0900

    ~lecture 10

--- page-routing-app/eslint.config.mjs
+++ page-routing-app/eslint.config.mjs
@@ -1,25 +1,47 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
+	...compat.extends('next/core-web-vitals'),
+	{
+		files: ['**/*.js', '**/*.jsx', '**/*.tsx'],
+		plugins: { prettier: prettierPlugin },
+		rules: {
+			'no-undef': 'error',
+			'prettier/prettier': [
+				'error',
+				{
+					singleQuote: true,
+					semi: true,
+					useTabs: true,
+					tabWidth: 2,
+					trailingComma: 'all',
+					printWidth: 80,
+					bracketSpacing: true,
+					arrowParens: 'avoid',
+					endOfLine: 'auto', 
+				},
+			],
+		},
+	},
+	{
+		ignores: [
+			'node_modules/**',
+			'.next/**',
+			'out/**',
+			'build/**',
+			'next-env.d.ts',
+		],
+	},
 ];
 
 export default eslintConfig;
---
+++ page-routing-app/src/pages/search/index.tsx
@@ -0,0 +1,9 @@
+import { useRouter } from 'next/router';
+
+export default function Page() {
+	const router = useRouter();
+
+	const { q } = router.query;
+
+	return <div>Search</div>;
+}
