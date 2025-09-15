9cf234eaab20ba3ccacac561443c83580d0115d4
worldbrighteststar <scott.choi@gmail.com>
2025-09-16 00:14:24 +0900

    ~lecture 13

--- page-routing-app/src/pages/_app.tsx
+++ page-routing-app/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
 	const router = useRouter();
@@ -10,12 +11,22 @@ export default function App({ Component, pageProps }: AppProps) {
 		router.push('/test');
 	};
 
+	/**
+	 * Pages are prefeched on Link components. (exc. prefetch=false)
+	 * It can be prefeched like bellow.
+	 */
+	useEffect(() => {
+		router.prefetch('/test');
+	}, []);
+
 	return (
 		<div>
 			<header>
 				<Link href={'/'}>index</Link>
 				&nbsp;
-				<Link href={'/search'}>search</Link>
+				<Link href={'/search'} prefetch={false}>
+					search
+				</Link>
 				&nbsp;
 				<Link href={'/book/1'}>book/1</Link>
 				<div>
--- page-routing-app/src/pages/index.tsx
+++ page-routing-app/src/pages/index.tsx
@@ -1,3 +1,10 @@
+import styles from './index.module.css';
+
 export default function index() {
-	return <h1>인덱스</h1>;
+	return (
+		<>
+			<h1 className={styles.h1}>인덱스</h1>
+			<h2 className={styles.h2}>인덱스</h2>
+		</>
+	);
 }
