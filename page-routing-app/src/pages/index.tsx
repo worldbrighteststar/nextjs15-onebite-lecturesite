915d76fd4bf4c998e4def8bbc4d60cc5de5336e9
worldbrighteststar <scott.choi@gmail.com>
2025-09-25 01:27:51 +0900

    ~lecture 17

--- page-routing-app/src/pages/index.tsx
+++ page-routing-app/src/pages/index.tsx
@@ -3,8 +3,19 @@ import styles from './index.module.css';
 import { ReactNode } from 'react';
 import books from '@/mock/books.json';
 import BookItem from '@/components/book-item';
+import { InferGetServerSidePropsType } from 'next';
 
-export default function Home() {
+export const getServerSideProps = () => {
+	// executed before component, fetching data from server
+	const data = 'hello';
+	return { props: { data } };
+};
+
+export default function Home({
+	data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+	console.log(data);
+
 	return (
 		<div className={styles.container}>
 			<section>
