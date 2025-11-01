469680d83e7188aa1c55f8292b451e600aba5bf5
worldbrighteststar <scott.choi1993@gmail.com>
2025-11-01 18:25:58 +0900

    ~lecture 46

--- app-routing-app/src/app/(with-searchbar)/search/loading.tsx
+++
@@ -1,7 +0,0 @@
-/**
- * Loading.tsx doesn't affect when the only query string's changed
- */
-
-export default function Loading() {
-	return <div>Loading ...</div>;
-}
--- app-routing-app/src/app/(with-searchbar)/search/page.tsx
+++ app-routing-app/src/app/(with-searchbar)/search/page.tsx
@@ -2,15 +2,12 @@ import books from '@/mock/books.json';
 import BookItem from '@/components/book-item';
 import { BookData } from '@/types';
 import delay from '@/util/delay';
+import { Suspense } from 'react';
 
-export default async function Page({
-	searchParams,
-}: {
-	searchParams: Promise<{ q: string }>;
-}) {
+async function SearchResult({ q }: { q: string }) {
 	await delay(1500);
 	const response = await fetch(
-		`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${(await searchParams).q}`,
+		`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`,
 		{ cache: 'force-cache' },
 	);
 	if (!response.ok) {
@@ -24,5 +21,20 @@ export default async function Page({
 				<BookItem key={book.id} {...book} />
 			))}
 		</div>
+	);
+}
+
+export default async function Page({
+	searchParams,
+}: {
+	searchParams: Promise<{ q: string }>;
+}) {
+	return (
+		<Suspense
+			key={(await searchParams).q || ''}
+			fallback={<div>Loading...</div>}
+		>
+			<SearchResult q={(await searchParams).q || ''} />
+		</Suspense>
 	);
 }
